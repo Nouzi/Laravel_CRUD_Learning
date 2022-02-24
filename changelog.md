4dc9fc4184b7e97804ddd7da6feeb2257d7288bd
Nouzi <77692484+Nouzi@users.noreply.github.com>
2022-02-24 15:41:50 +0100

    Initial commit

---
+++ changelog.md
@@ -0,0 +1,5 @@
+# Daily progress updates
+
+**24.2.2022**
+- Vytvorený projekt v Laravel 9 
+- Integrovaný [Tailwind css](https://tailwindcss.com).
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <title>Laravel</title>
+
+
+    </head>
+
+    <body>
+      <h1 class="text-3xl font-bold text-red-500 text-center mt-8"> Hello world </h1>
+    </body>
+</html>
